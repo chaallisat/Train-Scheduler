4744d3e437d2aee13c946fa42d2ca3b7115a5324
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-06-25 17:58:46 -0700

    initial files

---
+++ assets/javascript/moment.js
@@ -0,0 +1,19 @@
+//The core Firebase JS SDK is always required and must be listed first
+<script src="https://www.gstatic.com/firebasejs/6.2.3/firebase.js"></script>
+
+// TODO: Add SDKs for Firebase products that you want to use
+//     https://firebase.google.com/docs/web/setup#config-web-app
+
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCKC2JH5V3tiOkjuXsJLPEnO-BNaUKYJfI",
+    authDomain: "homework-7-train-71f26.firebaseapp.com",
+    databaseURL: "https://homework-7-train-71f26.firebaseio.com",
+    projectId: "homework-7-train-71f26",
+    storageBucket: "",
+    messagingSenderId: "795674173285",
+    appId: "1:795674173285:web:d23258c9ef568a56"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
