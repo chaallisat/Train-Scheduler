46e8a4ec9243e2004f855d231ecb09e9ed1e73e9
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-06-29 10:13:34 -0700

    form added to html and js update

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -14,3 +14,48 @@
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  const database = firebase.database()
+
+  // Initial Variables
+  let tName = "Hogwarts Express";
+  let destination = "Hogwarts";
+  let tFrequency = 3;
+  let firstTime = "03:30";
+
+//    * Train Name
+    
+//    * Destination 
+    
+//    * First Train Time -- in military time
+    
+//    * Frequency -- in minutes
+  
+//  * Code this app to calculate when the next train will arrive; this should be relative to the current time.
+  
+//  * Users from many different machines must be able to view same train times.
+
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    let firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    let currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    let tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    let tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    let nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
